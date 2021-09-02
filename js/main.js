e3dba6540d71050aa3da6928a266b5d2848b9b60
mohaiminul69 <mohaiminul69@gmail.com>
2021-09-02 12:05:12 +0600

    almost done

--- js/main.js
+++ js/main.js
@@ -8,38 +8,51 @@ document.getElementById("searchInput").addEventListener("keyup", (e) => {
 const getInputText = () => {
   const inputText = document.getElementById("searchInput").value;
   document.getElementById("searchInput").value = "";
+  searchResultCleaner("none");
   loadingToggler("block");
-  //   searchResultCleaner("none");
   getBooks(inputText);
 };
 const loadingToggler = (displayClass) => {
   document.getElementById("loadingBtn").style.display = displayClass;
 };
 const searchResultCleaner = (displayClass) => {
-  document.getElementById("book-list").style.display = displayClass;
+  document.getElementById("book-list-container").style.display = displayClass;
 };
 
 const getBooks = async (searchText) => {
-  const url = `http://openlibrary.org/search.json?q=${searchText}`;
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayBooks(data.docs);
+  displayBooks(data);
 };
 
-const displayBooks = (books) => {
+const displayBooks = (data) => {
+  const books = data.docs;
+  const numberofBooksFound = data.numFound;
+  const bookList = document.getElementById("book-list");
+  const h5 = document.getElementById("numberofBooks");
+  if (numberofBooksFound > 100) {
+    h5.innerHTML = `Showing <span class="text-blue">100</span> out of <span class="text-blue">${numberofBooksFound}</span> Found Books`;
+  } else if (numberofBooksFound === 1) {
+    h5.innerHTML = `Showing <span class="text-blue">${numberofBooksFound}</span> Book in the List`;
+  } else if (numberofBooksFound === 0) {
+    h5.innerHTML = `<span class="text-danger">Sorry no books found by this name . . .</span>`;
+  } else {
+    h5.innerHTML = `Showing <span class="text-blue">${numberofBooksFound}</span> Books in the List`;
+  }
+  bookList.textContent = "";
   books.forEach((book) => {
     console.log(book);
-    const bookList = document.getElementById("book-list");
     const div = document.createElement("div");
     div.classList.add("col", "rounded-3", "py-3", "mt-2");
     div.innerHTML = `
-            <div class="custom-card">
+            <div class="card h-100">
             ${
               book.cover_i
-                ? `<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="Image Not Found" />`
-                : "Image Not Found"
+                ? `<img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid card-img-top" alt="Image Not Found" />`
+                : `<img src="./img/image-not-found-scaled-1150x647.png" class="img-fluid card-img-top" />`
             }              
-              <div class="card-body d-flex flex-column justify-content-end">
+              <div class="card-body d-flex flex-column justify-content-start">
                 <h5 class="card-title">Book Name: ${book.title}</h5>
                 <h5 class="card-title">Author Name: ${
                   book.author_name ? book.author_name : "Author not found"
@@ -58,5 +71,5 @@ const displayBooks = (books) => {
     bookList.appendChild(div);
   });
   loadingToggler("none");
-  //   searchResultCleaner("block");
+  searchResultCleaner("block");
 };
